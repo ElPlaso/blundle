7ba8d5d0c3c0873b7baa9cfafb22c094201a7a13
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2023-11-28 22:53:38 +1300

    allow theme to be reloaded

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,14 +1,23 @@
 import { IconButton } from "@mui/material";
 import { Brightness4 } from "@mui/icons-material";
+import { useEffect } from "react";
 
 export default function ThemeToggle() {
-  function handleClick() {
+  useEffect(() => {
     if (localStorage.theme === "dark" || !("theme" in localStorage)) {
       document.documentElement.classList.add("dark");
-      localStorage.theme = "light";
+    } else {
+      document.documentElement.classList.remove("light");
+    }
+  }, []);
+
+  function handleClick() {
+    if (localStorage.theme === "light" || !("theme" in localStorage)) {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
     } else {
       document.documentElement.classList.remove("dark");
-      localStorage.theme = "dark";
+      localStorage.theme = "light";
     }
   }
 
