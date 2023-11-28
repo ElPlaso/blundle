e65dad25c104aa582cb7714271b3683f7856ea04
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2023-11-28 22:37:49 +1300

    fix unneeded set theme

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,22 +1,17 @@
 import { IconButton } from "@mui/material";
 import { Brightness4 } from "@mui/icons-material";
 
-const setTheme = () => {
-  if (localStorage.theme === "dark" || !("theme" in localStorage)) {
-    document.documentElement.classList.add("dark");
-    localStorage.theme = "light";
-  } else {
-    document.documentElement.classList.remove("dark");
-    localStorage.theme = "dark";
-  }
-};
-
 export default function ThemeToggle() {
-  setTheme();
-
   function handleClick() {
-    setTheme();
+    if (localStorage.theme === "dark" || !("theme" in localStorage)) {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "light";
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "dark";
+    }
   }
+
   return (
     <IconButton size="large" onClick={handleClick} disableRipple>
       <Brightness4 fontSize="large" className="text-black dark:text-white" />
