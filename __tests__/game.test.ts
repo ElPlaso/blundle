e414ed1a30fa4677c01da587fd6358d1448321e8
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-02-15 17:13:19 +1300

    Add failing test

--- __tests__/game.test.ts
+++ __tests__/game.test.ts
@@ -30,6 +30,15 @@ describe("Game", () => {
     expect(result.incorrectButIncludedMoves).toEqual([1, 3]);
   });
 
+  test("Compares current guess to solution, partially correct with duplicate move", () => {
+    const currentGuessMoves = ["a1", "d4", "c4", "b2"];
+    const solution = ["a1", "a1", "c3", "d4"];
+    const numberOfMovesPerGuess = 4;
+    const result = compareGuessToSolution(currentGuessMoves, solution, numberOfMovesPerGuess);
+    expect(result.correctMoves).toEqual([0]);
+    expect(result.incorrectButIncludedMoves).toEqual([3]);
+  });
+
   test("Allows any checkmate as long as rest of sequence is correct", () => {
     const currentGuessMoves = ["a1", "b2", "c3", "d5#"];
     const solution = ["a1", "b2", "c3", "d4#"];
