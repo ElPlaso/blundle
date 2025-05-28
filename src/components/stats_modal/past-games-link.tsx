b6c146461e46e8499d5e8c3d4aab89791e111ea4
Moo Plah Soe Kusay <plahsoekusay@gmail.com>
2025-05-28 22:19:30 +1200

    Add link to past puzzles page

--- src/components/stats_modal/past-games-link.tsx
+++ src/components/stats_modal/past-games-link.tsx
@@ -1,10 +1,13 @@
 import { Extension } from "@mui/icons-material";
+import { Link } from "react-router";
 
 export default function PastGamesLink() {
   return (
-    <button className="hover: flex w-48 justify-center gap-x-2 rounded-full py-2 text-center text-sm font-bold text-white enabled:bg-lightPresent disabled:cursor-not-allowed disabled:bg-lightAbsent enabled:dark:bg-darkPresent dark:disabled:bg-darkAbsent">
-      Past Puzzles
-      <Extension />
-    </button>
+    <Link to="/past-puzzles">
+      <button className="flex justify-center w-48 py-2 text-sm font-bold text-center text-white rounded-full hover: gap-x-2 enabled:bg-lightPresent disabled:cursor-not-allowed disabled:bg-lightAbsent enabled:dark:bg-darkPresent dark:disabled:bg-darkAbsent">
+        Past Puzzles
+        <Extension />
+      </button>
+    </Link>
   );
 }
